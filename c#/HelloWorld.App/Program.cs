f053f92990b4a16c3f52df18c3d60bab54ba7146
Marc Roethlisberger <m.roethlisberger477@gmail.com>
2025-02-04 16:24:37 +0100

    Added basic setup with integrated test suite

---
+++ c#/HelloWorld.App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld.App;
+public class Program
+
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static string GiveMeSomeString(){
+        return "Test";
+    }
+}
